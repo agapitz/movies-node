e32e1c61c2704eae286c7ce03ebffed771566efb
Ronie Mer <pitz@Inventlabss-MacBook-Pro.local>
2022-06-22 04:04:39 +0800

    onlin db

--- dbcon.js
+++ dbcon.js
@@ -1,18 +1,26 @@
 var mysql = require('mysql');
 
 
+// var connection = mysql.createPool({
+
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'testDB'
+
+// });
 var connection = mysql.createPool({
 
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'testDB'
+    host: '206.189.87.175',
+    user: 'bossronie',
+    port: 3306,
+    password: 'bossronie123$',
+    database: 'tetDB'
 
 });
-
-// connection.query('select * from movies order by showing_start', (error, results, fields) => {
-//     if (error) throw error;
-//     console.log('The solution is: ', results);
-// });
+connection.query('select * from movies order by showing_start', (error, results, fields) => {
+    if (error) throw error;
+    console.log('The solution is: ', results);
+});
 
 module.exports = connection;
